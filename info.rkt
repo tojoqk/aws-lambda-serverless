e83576fa2eb9cc199a8e4bad0c16a1bedf3f932e
Masaya Tojo <masaya@tojo.tokyo>
2019-01-20 17:09:42 +0900

    edit pkg-desc

--- info.rkt
+++ info.rkt
@@ -3,6 +3,6 @@
 (define deps '("base" "yaml" "aws"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/aws-lambda-serverless.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "Racket on serverless framework")
 (define version "0.0")
 (define pkg-authors '(tojoqk))
