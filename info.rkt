f89efe531d43509384944502f1e2a996b8e2dbef
Masaya Tojo <masaya@tojo.tokyo>
2019-01-20 17:09:59 +0900

    Version Up!

--- info.rkt
+++ info.rkt
@@ -4,5 +4,5 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/aws-lambda-serverless.scrbl" ())))
 (define pkg-desc "Racket on serverless framework")
-(define version "0.0")
+(define version "1.0")
 (define pkg-authors '(tojoqk))
