7d093c302d4756dd3b913f508cf62321e76a2202
Masaya Tojo <masaya@tojo.tokyo>
2019-07-17 06:41:32 +0900

    Remove aws package

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "aws-lambda-serverless")
-(define deps '("base" "yaml" "aws"))
+(define deps '("base" "yaml"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/aws-lambda-serverless.scrbl" ())))
 (define pkg-desc "Racket on aws lambda with serverless framework.")
